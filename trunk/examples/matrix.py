d028b6a1ba59816066304e0c2ca491139eab96da
Taura.Kenjiro@gmail.com <Taura.Kenjiro@gmail.com@01341d6f-fc3b-c789-1391-2716b97b5038>
2012-06-01 13:27:46 +0000

    initial commit

---
+++ trunk/examples/matrix.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+import sys
+
+sys.path.append("")
+
+import smart_gnuplotter
+reload(smart_gnuplotter)
+
+g = smart_gnuplotter.smart_gnuplotter()
+g.default_terminal = 'epslatex color size 10cm,5cm font "" 10'
+
+db = "matrix.sqlite"
+
+Ms    = g.do_sql(db, "select distinct M from a where M > 500", single_col=1)
+archs = g.do_sql(db, "select distinct arch from a", single_col=1)
+parallel_types = g.do_sql(db, "select distinct type from a where ppn > 1", 
+                          single_col=1)
+
+if 1:
+    query = r'''select type,avg(gflops_per_sec) from a 
+where M = %(M)s and ppn = 1 and arch = "%(arch)s" 
+group by type'''
+    g.graphs((db, query), 
+             output="graphs/serial_%(arch)s_%(M)s.tex",
+             curve_title="",
+             curve_with="boxes fs pattern 1",
+             boxwidth="0.9 relative",
+             graph_title="%(arch)s M=%(M)s",
+             yrange="[0:]",
+             xlabel="Program",
+             ylabel="Performance (GFLOPS)",
+             M=Ms, arch=archs, graph_vars=[ "arch", "M" ])
+    
+if 1:
+    query = r'''select ppn,avg(gflops_per_sec) from a 
+where type="%(typ)s" and M=%(M)s and arch="%(arch)s" 
+group by ppn'''
+    g.graphs((db, query), 
+             output="graphs/gflops_%(arch)s_%(M)s.tex",
+             curve_title="%(typ)s",
+             curve_with="linespoints",
+             graph_title="%(arch)s M=%(M)s",
+             xrange="[0:]",
+             yrange="[0:]",
+             xlabel="cores",
+             ylabel="Performance (GFLOPS)",
+             typ=parallel_types, M=Ms, arch=archs, graph_vars=[ "arch", "M" ])
+    
+if 1:
+    init = r'''
+create temp table serial as 
+select arch,M,avg(gflops_per_sec) serial_gflops_per_sec 
+from a where type = "serial" 
+group by arch,M;
+
+create temp table b as select * from serial natural join a;
+'''
+    query = r'''select ppn,avg(gflops_per_sec / serial_gflops_per_sec) from b
+where type="%(typ)s" and M=%(M)s and arch="%(arch)s" group by ppn'''
+    g.graphs((db, query, init), 
+             output="graphs/speedup_%(arch)s_%(M)s.tex",
+             curve_title="%(typ)s",
+             curve_with="linespoints",
+             graph_title="%(arch)s M=%(M)s",
+             xrange="[0:]",
+             yrange="[0:]",
+             xlabel="cores",
+             ylabel="speedup",
+             typ=parallel_types, M=Ms, arch=archs, graph_vars=[ "arch", "M" ])
+
+    g.graphs((db, query, init), 
+             overlays=[("x",'title "ideal"')],
+             output="graphs/speedup_with_ideal_%(arch)s_%(M)s.tex",
+             gpl_file="graphs/speedup_with_ideal_%(arch)s_%(M)s.gpl",
+             curve_title="%(typ)s",
+             curve_with="linespoints",
+             graph_title="%(arch)s M=%(M)s",
+             xrange="[0:]",
+             yrange="[0:]",
+             xlabel="cores",
+             ylabel="speedup",
+             typ=parallel_types, M=Ms, arch=archs, graph_vars=[ "arch", "M" ])
+    
+
+
+
